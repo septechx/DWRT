01d1e1b3344d18775b60a3ce81a3838007e311f0
Separatehhh23 <separatehhh23@gmail.com>
2024-12-22 18:30:03 +0100

    Fix adding and removing members

--- src/main/java/com/siesque/dwrt/Team.java
+++ src/main/java/com/siesque/dwrt/Team.java
@@ -3,6 +3,8 @@ package com.siesque.dwrt;
 import java.util.ArrayList;
 import java.util.List;
 
+import com.siesque.dwrt.database.TeamStorage;
+
 public class Team {
     public final String name;
     public List<String> members = new ArrayList<>();
@@ -14,9 +16,26 @@ public class Team {
 
     public void addMember(String member) {
         members.add(member);
+        write();
     }
 
     public void removeMember(String member) {
         members.remove(member);
+        write();
+    }
+
+    private void write() {
+        List<Team> prevTeams = TeamStorage.loadData();
+        List<Team> teams = new ArrayList<>();
+
+        for (Team team : prevTeams) {
+            if (team.name.equals(name)) {
+                teams.add(this);
+            } else {
+                teams.add(team);
+            }
+        }
+
+        TeamStorage.saveData(teams);
     }
 }
